e1e054f0e352acc7ab0c6a49902c7a4d89ef55ac
Steven <stevenhovater@gmail.com>
2020-07-13 17:46:01 -0500

    Initial Commit

---
+++ Slide and Swipe Concept/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slide and Swipe Concept
+//
+//  Created by Steven Hovater on 7/13/20.
+//  Copyright © 2020 Steven Hovater. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
